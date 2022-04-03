58224c6e2a2edfd8b08720019ec185fd01f58305
hhosaka <hhosaka183@gmail.com>
2022-04-03 22:37:51 +0900

    Supported LifePoint

--- templates/Games/players.php
+++ templates/Games/players.php
@@ -7,18 +7,23 @@
             <?= 'Rule : '.$outline->RuleName?><br>
             <table border='1'>
                 <tr>
+                <?php if($outline->EnableLifePoint):?>
                     <th colspan=5></th>
                     <th colspan=3>Opponent</th>
+                <?php else:?>
+                    <th colspan=4></th>
+                    <th colspan=2>Opponent</th>
+                <?php endif;?>
                 </tr>
                 <tr>
                     <th>order</th>
                     <th>name</th>
                     <th>Match Point</th>
                     <th>Win Point</th>
-                    <th>Life Point</th>
+                    <?php if($outline->EnableLifePoint):?><th>Life Point</th> ?><?php endif;?>
                     <th>Match Point</th>
                     <th>Win Point</th>
-                    <th>Life Point</th>
+                    <?php if($outline->EnableLifePoint):?><th>Life Point</th><?php endif;?>
                 </tr>
                 <?php foreach ($players as $player):?>
                     <tr>
@@ -26,10 +31,10 @@
                         <td><?= h($player->Name) ?></td>
                         <td style="text-align: center;"><?= h($player->Point->MatchPoint) ?></td>
                         <td style="text-align: center;"><?= h(round($player->Point->WinPoint, 2)) ?></td>
-                        <td style="text-align: center;"><?= h($player->Point->LifePoint) ?></td>
+                        <?php if($outline->EnableLifePoint):?><td style="text-align: center;"><?= h($player->Point->LifePoint) ?></td><?php endif;?>
                         <td style="text-align: center;"><?= h($player->OpponentPoint->MatchPoint) ?></td>
                         <td style="text-align: center;"><?= h(round($player->OpponentPoint->WinPoint,2)) ?></td>
-                        <td style="text-align: center;"><?= h($player->OpponentPoint->LifePoint) ?></td>
+                        <?php if($outline->EnableLifePoint):?><td style="text-align: center;"><?= h($player->OpponentPoint->LifePoint) ?></td><?php endif;?>
                 </tr>
                 <?php endforeach;?>
             </table>
